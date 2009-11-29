3f299113ad6f7a3490defdc2f092228a00cba9f2
apj.ali <apj.ali@5b3350a4-d481-11de-aee2-4541d0e33e4f>
2009-11-29 13:08:36 +0000

    ¡Ya tenemos log!

--- src/control/juegos/ovejas_lobos/OvejasLobosDemo.java
+++ src/control/juegos/ovejas_lobos/OvejasLobosDemo.java
@@ -6,59 +6,113 @@ package control.juegos.ovejas_lobos;
 
 import aima.search.framework.TreeSearch;
 import aima.search.uninformed.*;
+import java.util.logging.Level;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author Alicia
  */
-
 public class OvejasLobosDemo {
 
+    private final static Logger log = Logger.getLogger(OvejasLobosDemo.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("\n\n\n**************************************");
-        System.out.println("     BÚSQUEDA PRIMERO EN ANCHURA");
-        System.out.println("**************************************\n");
+
         BreadthFirstDemo();
-        System.out.println("\n\n\n**************************************");
-        System.out.println("    BÚSQUEDA PRIMERO EN PROFUNDIDAD");
-        System.out.println("**************************************\n");
         DepthFirstSearchDemo();
-        System.out.println("\n\n\n**************************************");
-        System.out.println(" BÚSQUEDA EN PROFUNDIDAD LIMITADA A 11");
-        System.out.println("**************************************\n");
         DepthLimitedSearchDemo();
-        System.out.println("\n\n\n**************************************");
-        System.out.println("       BÚSQUEDA DE COSTE UNIFORME");
-        System.out.println("**************************************\n");
         UniformCostSearchDemo();
-        System.out.println("\n\n\n**************************************");
-        System.out.println("           BÚSQUEDA ¿ITERATIVA?");
-        System.out.println("**************************************\n");
         IterativeDeepeningSearchDemo();
     }
 
     private static void BreadthFirstDemo() {
-        OvejasLobosJuego juego = new OvejasLobosJuego(new BreadthFirstSearch(new TreeSearch()));
-        juego.ejecutar();
+
+        OvejasLobosJuego juego;
+
+        try {
+            juego = new OvejasLobosJuego(new BreadthFirstSearch(new TreeSearch()));
+
+            log.info("\n\n\n**************************************");
+            log.info("     BÚSQUEDA PRIMERO EN ANCHURA");
+            log.info("**************************************\n");
+
+            juego.ejecutar();
+
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(OvejasLobosDemo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     private static void DepthFirstSearchDemo() {
-        OvejasLobosJuego juego = new OvejasLobosJuego(new DepthFirstSearch(new TreeSearch()));
-        juego.ejecutar();
+
+        OvejasLobosJuego juego;
+
+        try {
+            juego = new OvejasLobosJuego(new DepthFirstSearch(new TreeSearch()));
+
+            log.info("\n\n\n**************************************");
+            log.info("    BÚSQUEDA PRIMERO EN PROFUNDIDAD");
+            log.info("**************************************\n");
+
+            juego.ejecutar();
+
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(OvejasLobosDemo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-   private static void DepthLimitedSearchDemo() {
-        OvejasLobosJuego juego = new OvejasLobosJuego(new DepthLimitedSearch(11));
-        juego.ejecutar();
+    private static void DepthLimitedSearchDemo() {
+
+        OvejasLobosJuego juego;
+
+        try {
+            juego = new OvejasLobosJuego(new DepthLimitedSearch(11));
+
+            log.info("\n\n\n**************************************");
+            log.info(" BÚSQUEDA EN PROFUNDIDAD LIMITADA A 11");
+            log.info("**************************************\n");
+
+            juego.ejecutar();
+
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(OvejasLobosDemo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-      private static void UniformCostSearchDemo() {
-        OvejasLobosJuego juego = new OvejasLobosJuego(new UniformCostSearch(new TreeSearch()));
-        juego.ejecutar();
+    private static void UniformCostSearchDemo() {
+
+        OvejasLobosJuego juego;
+
+        try {
+            juego = new OvejasLobosJuego(new UniformCostSearch(new TreeSearch()));
+
+            log.info("\n\n\n**************************************");
+            log.info("       BÚSQUEDA DE COSTE UNIFORME");
+            log.info("**************************************\n");
+
+            juego.ejecutar();
+
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(OvejasLobosDemo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     private static void IterativeDeepeningSearchDemo() {
-        OvejasLobosJuego juego = new OvejasLobosJuego(new IterativeDeepeningSearch());
-        juego.ejecutar();
+
+        OvejasLobosJuego juego;
+
+        try {
+            juego = new OvejasLobosJuego(new IterativeDeepeningSearch());
+
+            log.info("\n\n\n**************************************");
+            log.info("           BÚSQUEDA ITERATIVA");
+            log.info("**************************************\n");
+
+            juego.ejecutar();
+
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(OvejasLobosDemo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/control/juegos/ovejas_lobos/OvejasLobosEstadoObjetivo.java
+++ src/control/juegos/ovejas_lobos/OvejasLobosEstadoObjetivo.java
@@ -2,17 +2,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package control.juegos.ovejas_lobos;
 
 import aima.search.framework.GoalTest;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Alicia
  */
-public class OvejasLobosEstadoObjetivo  implements GoalTest {
+public class OvejasLobosEstadoObjetivo implements GoalTest {
 
     private OvejasLobosJuego _juego;
 
@@ -22,16 +23,22 @@ public class OvejasLobosEstadoObjetivo  implements GoalTest {
 
     public boolean isGoalState(Object arg0) {
         HashMap estado = new HashMap(3);
-        boolean ok;
+        boolean ok = false;
 
-        estado.put(OvejasLobosEstado.LOBOS, 0);
-        estado.put(OvejasLobosEstado.OVEJAS, 0);
-        estado.put(OvejasLobosEstado.CANOA, 1);
+        try {
+            estado.put(OvejasLobosEstado.LOBOS, 0);
+            estado.put(OvejasLobosEstado.OVEJAS, 0);
+            estado.put(OvejasLobosEstado.CANOA, 1);
 
-        ok = arg0.equals(new OvejasLobosEstado(estado));
+            ok = arg0.equals(new OvejasLobosEstado(estado));
 
-        if (ok) {
-            this._juego.setSolucion(ok);
+            if (ok) {
+                this._juego.setSolucion(ok);
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(OvejasLobosEstadoObjetivo.class.getName()).log(Level.SEVERE,
+                    "Error al comparar el estado " + arg0.toString() + " con el estado final.", ex);
         }
 
         return ok;
