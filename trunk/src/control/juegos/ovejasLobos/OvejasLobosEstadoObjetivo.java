a0c153d47494bd90e114b42838042adfaf892b1e
apj.ali <apj.ali@5b3350a4-d481-11de-aee2-4541d0e33e4f>
2009-12-10 23:46:09 +0000

    Ovejas y Lobos desaparecidos!!

---
+++ trunk/src/control/juegos/ovejasLobos/OvejasLobosEstadoObjetivo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.juegos.ovejas_lobos;
+
+import aima.search.framework.GoalTest;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Alicia
+ */
+public class OvejasLobosEstadoObjetivo implements GoalTest {
+
+    public boolean isGoalState(Object arg0) {
+        HashMap estado = new HashMap(3);
+        boolean ok = false;
+
+        try {
+            estado.put(OvejasLobosEstado.LOBOS, 0);
+            estado.put(OvejasLobosEstado.OVEJAS, 0);
+            estado.put(OvejasLobosEstado.CANOA, 1);
+
+            ok = arg0.equals(new OvejasLobosEstado(estado));
+
+        } catch (Exception ex) {
+            Logger.getLogger(OvejasLobosEstadoObjetivo.class.getName()).log(Level.SEVERE,
+                    "Error al comparar el estado " + arg0.toString() + " con el estado final.", ex);
+        }
+
+        return ok;
+    }
+}
