cb779b50e264a0bf0ddf2531289d240505ab6cbf
apj.ali <apj.ali@5b3350a4-d481-11de-aee2-4541d0e33e4f>
2009-12-10 23:46:09 +0000

    Ovejas y Lobos desaparecidos!!

---
+++ src/control/juegos/ovejasLobos/OvejasLobosFuncionSucesor.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.juegos.ovejas_lobos;
+
+import aima.search.framework.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Alicia
+ */
+public class OvejasLobosFuncionSucesor implements SuccessorFunction {
+
+    public List getSuccessors(Object arg0) {
+
+        ArrayList resultado = new ArrayList();
+        OvejasLobosEstado estadoPadre = (OvejasLobosEstado) arg0;
+        ArrayList recorrido = estadoPadre.getRecorrido();
+
+        try {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    OvejasLobosEstado estado = new OvejasLobosEstado(estadoPadre);
+                    estado.setRecorrido(recorrido);
+                    if (estado.mover(i, j)) {
+                        recorrido = estado.getRecorrido();
+                        resultado.add(new Successor("                  (" + i + "," + j + ")" + estado.toString(),
+                                estado));
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(OvejasLobosFuncionSucesor.class.getName())
+                    .log(Level.SEVERE, "Error al obtener los sucesores de " + arg0.toString(), ex);
+        }
+
+        return resultado;
+    }
+}
