2aec3918cfe72f9730129159b100d152435a3bb3
apj.ali <apj.ali@5b3350a4-d481-11de-aee2-4541d0e33e4f>
2010-01-17 18:15:40 +0000

    Corrección Garrafas

--- src/control/juegos/Garrafas/GarrafasEstado.java
+++ src/control/juegos/Garrafas/GarrafasEstado.java
@@ -6,6 +6,7 @@ import java.util.Vector;
  * @author Grupo C15
  */
 public class GarrafasEstado {
+
     /**
      * Numero de litros que se desean obtener en la garrafa cuya capacidad son 4 litros
      */
@@ -26,6 +27,7 @@ public class GarrafasEstado {
      * litros de capacidad
      */
     private Vector<GarrafasContenido> _contenido;
+
     /**
      * Constructor por defecto
      * Siendo el estado inicial, ambas garrafas vacías
@@ -35,6 +37,7 @@ public class GarrafasEstado {
         this._garrafa4 = 0;
         this._contenido = new Vector<GarrafasContenido>();
     }
+
     /**
      * Constructor parametrizado
      * @param garrafa3 GarrafasContenido de la garrafa de 3 litros de capacidad
@@ -76,9 +79,10 @@ public class GarrafasEstado {
      * @return  TRUE en caso de ser el estado objetivo
      *          FALSE en cualquier otro caso
      */
-    public boolean esObjetivo(){
+    public boolean esObjetivo() {
         return this.getGarrafa3() == NLITROS;
     }
+
     /**
      * Revisamos cada uno de los movimientos que se han realizado entre las 2
      * garrafas, si ya se ha realizado no tiene sentido volver a ejecutarlo, por
@@ -87,15 +91,169 @@ public class GarrafasEstado {
      * @param posibleG4 Posible modificación del contenido de la garrafa4
      * @return Indica si dicha modificación ya se ha realizado o no
      */
-    public boolean cambioAunNoRealizado(int posibleG3, int posibleG4){
+    public boolean cambioAunNoRealizado(int posibleG3, int posibleG4) {
         boolean posible = true;
         int i = 0;
-        while (posible && (i < this.getContenido().size())){
-            if((this.getContenido().get(i).getContG3() == posibleG3)&&
-               (this.getContenido().get(i).getContG4() == posibleG4))
+        while (posible && (i < this.getContenido().size())) {
+            if ((this.getContenido().get(i).getContG3() == posibleG3)
+                    && (this.getContenido().get(i).getContG4() == posibleG4)) {
                 posible = false;
+            }
             i++;
         }
         return posible;
     }
+
+    public String toString(int g3, int g4) {
+        String resultado = "\n\n";
+        switch (g4) {
+            case 0:
+                switch (g3) {
+                    case 0:
+                        resultado += "                   |          |\n";
+                        resultado += " |          |      |          |\n";
+                        resultado += " |          |      |          |\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 1:
+                        resultado += "                   |          |\n";
+                        resultado += " |          |      |          |\n";
+                        resultado += " |____|      |          |\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 2:
+                        resultado += "                   |          |\n";
+                        resultado += " |____|      |          |\n";
+                        resultado += " |____|      |          |\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 3:
+                        resultado += "  ____       |          |\n";
+                        resultado += " |____|      |          |\n";
+                        resultado += " |____|      |          |\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                }
+                break;
+            case 1:
+                switch (g3) {
+                    case 0:
+                        resultado += "                   |          |\n";
+                        resultado += " |          |      |          |\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 1:
+                        resultado += "                   |          |\n";
+                        resultado += " |          |      |          |\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 2:
+                        resultado += "                   |          |\n";
+                        resultado += " |____|      |          |\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 3:
+                        resultado += "  ____       |          |\n";
+                        resultado += " |____|      |          |\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                }
+                break;
+            case 2:
+                switch (g3) {
+                    case 0:
+                        resultado += "                   |          |\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 1:
+                        resultado += "                   |          |\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 2:
+                        resultado += "                   |          |\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 3:
+                        resultado += "  ____       |          |\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                }
+                break;
+            case 3:
+                switch (g3) {
+                    case 0:
+                        resultado += "                   |____|\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 1:
+                        resultado += "                   |____|\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 2:
+                        resultado += "                         |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 3:
+                        resultado += "  ____       |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                }
+                break;
+            case 4:
+                switch (g3) {
+                    case 0:
+                        resultado += "                    ____\n";
+                        resultado += "                   |____|\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 1:
+                        resultado += "                    ____\n";
+                        resultado += "                   |____|\n";
+                        resultado += " |          |      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 2:
+                        resultado += "                    ____\n";
+                        resultado += "                   |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                    case 3:
+                        resultado += "                    ____\n";
+                        resultado += "  ____       |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        resultado += " |____|      |____|\n";
+                        break;
+                }
+                break;
+        }
+
+        return resultado;
+    }
+
 }
